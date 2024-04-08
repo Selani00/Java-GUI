655fc1700ebb1c5bc2006f0c7930154ddddc88c4
Selani00 <pkselani00@gmail.com>
2024-04-08 19:53:42 +0530

    Initial

---
+++ Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+
+class Main{
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        
+        frame.setTitle("First GUI App");
+
+        
+
+        frame.setVisible(true);
+    }
+}
