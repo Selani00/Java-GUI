d988f11d7ef1d36a974ee3848609c0c96a3fe5af
Selani00 <pkselani00@gmail.com>
2024-04-08 20:09:23 +0530

    Main UI

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*; // To import color class
 
 
 class Main{
@@ -7,8 +8,16 @@ class Main{
         
         frame.setTitle("First GUI App");
 
+        frame.setSize(420, 420);
+
+        // to treminate the window by closing the window
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         
 
         frame.setVisible(true);
+
+        // to give color
+        frame.getContentPane().setBackground(new Color(46,156,188));
     }
 }
